8e71f2cebfdb5fb1e11ef0c39b8a62080b164b13
Arongil <lakernewhouse@gmail.com>
2017-11-10 11:05:25 -0800

    Create Input.js

---
+++ javascript/input.js
@@ -0,0 +1,34 @@
+var Input = {
+  // 37 is the left arrow. 38 is the up arrow. 39 is the right arrow.
+  "37": false,
+  "38": false,
+  "39": false,
+  "40": false,
+  "32": false,
+  "click": false,
+  "mousepos": {
+    "x": 0,
+    "y": 99999
+  }
+};
+
+window.onkeydown = function(e) {
+  if (Input[e.keyCode] !== undefined) {
+    Input[e.keyCode] = true;
+  }
+};
+window.onkeyup = function(e) {
+  if (Input[e.keyCode] !== undefined) {
+    Input[e.keyCode] = false;
+  }
+};
+document.getElementById("canvas").onmouseup = function(e) {
+  Input["click"] = true;
+};
+document.onmousemove = function(e) {
+  var x = e.clientX - window.innerWidth/2;
+
+  var y = e.clientY - HALFHEIGHT - 8;
+  Input["mousepos"]["x"] = x;
+  Input["mousepos"]["y"] = y;
+};
