4665f3ec9901f5014de8a932b59b34e66e13f1a6
Arongil <lakernewhouse@gmail.com>
2017-11-10 10:58:44 -0800

    Main.js Framework

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,41 @@
+var canvas, ctx, WIDTH, HEIGHT, HALFWIDTH, HALFHEIGHT;
 
+function resize() {
+  ctx.translate(-HALFWIDTH, -HALFHEIGHT);
+  
+  canvas.width = 4/5 * window.innerWidth;
+  canvas.height = canvas.width;
+  if (canvas.height > 7/8 * window.innerHeight) {
+    // If the height is greater than the height of the screen, set it accordingly.
+    canvas.height = 7/8 * window.innerHeight;
+    canvas.width = canvas.height;
+  }
+  
+  WIDTH = canvas.width;
+  HEIGHT = canvas.height;
+  HALFWIDTH = WIDTH / 2;
+  HALFHEIGHT = HEIGHT / 2;
+  
+  ctx.translate(HALFWIDTH, HALFHEIGHT);
+}
+
+function init() {
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  
+  var body = document.getElementsByTagName("body")[0];
+  body.onresize = resize;
+  resize();
+  
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+  
+  ctx.translate(HALFWIDTH, HALFHEIGHT);
+  
+  loop();
+}
+
+loop() {
+  window.requestAnimationFrame(loop);
+}
+window.onload = init;
