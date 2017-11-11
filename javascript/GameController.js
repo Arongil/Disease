e4db921a87b89db1e4a94f3407f199a96bed120c
Arongil <lakernewhouse@gmail.com>
2017-11-11 08:02:37 -0800

    Agent instantiation refactor
    Updated land-only-spawning to use a do-while

--- javascript/GameController.js
+++ javascript/GameController.js
@@ -4,14 +4,11 @@ class GameController {
     this.agents = [];
     // temp *********
     for (var i = 0, x, y, color; i < 1000; i++) {
-      x = 2*(Math.random()-1/2) * WIDTH;
-      y = 2*(Math.random()-1/2) * HEIGHT;
-      color = colorAt(x, y);
-      while (color[0] < 200 || color[1] < 200 || color[2] < 200) {
+      do {
         x = 2*(Math.random()-1/2) * WIDTH;
         y = 2*(Math.random()-1/2) * HEIGHT;
         color = colorAt(x, y);
-      }
+      } while (color[0] < 200 || color[1] < 200 || color[2] < 200);
       
       this.agents.push(new Agent(
         new Vector2D(x, y),
