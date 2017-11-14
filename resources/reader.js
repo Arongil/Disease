e49982cb3265ca822ef0c16dc334d6a55d836951
the-inhuman-account <31903083+the-inhuman-account@users.noreply.github.com>
2017-11-14 15:08:00 -0800

    Delete reader.js

--- resources/reader.js
+++
@@ -1,9 +0,0 @@
-function read_csv(filename) {
-  const raw_file = new XMLHttpRequest();
-  raw_file.open('GET',filename,true);
-  raw_file.onreadystatechange = function(){
-    if(raw_file.readyState === 4 && [0,200].includes(raw_file.status)){
-      const allText = raw_file.reponseText;
-    }
-  };
-}
