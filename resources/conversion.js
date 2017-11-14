94689d141a09f26ed4fad201619226e823e2d923
Arongil <lakernewhouse@gmail.com>
2017-11-14 15:01:38 -0800

    Update conversion.js

--- resources/conversion.js
+++ resources/conversion.js
@@ -1,4 +1,4 @@
-const R = 6378.1 /* kilometers */, sq2 = Math.sqrt(2);
-function convert_coords(lng, lat) {
-  return new Vector2D(R * Math.PI * lng / (180 * sq2), R * sq2 * Math.sin(lat));
+const RADIUS = 6378.1 /* radius of Earth in kilometers */, sq2 = Math.sqrt(2);
+function convert_coords(latitude, longitude) {
+  return new Vector2D(RADIUS * Math.PI * longitude / (180 * sq2), RADIUS * sq2 * Math.sin(latitude));
 }
