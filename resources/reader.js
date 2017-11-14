7793cb0b312f1656209d16e41364aeb3477f2606
the-inhuman-account <31903083+the-inhuman-account@users.noreply.github.com>
2017-11-14 15:04:13 -0800

    Update reader.js

--- resources/reader.js
+++ resources/reader.js
@@ -4,7 +4,6 @@ function read_csv(filename) {
   raw_file.onreadystatechange = function(){
     if(raw_file.readyState === 4 && [0,200].includes(raw_file.status)){
       const allText = raw_file.reponseText;
-      alert(allText);
     }
   };
 }
