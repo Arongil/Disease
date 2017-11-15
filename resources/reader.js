be897489fed3d55f5257209275eda785e9d3e383
the-inhuman-account <31903083+the-inhuman-account@users.noreply.github.com>
2017-11-15 15:24:36 -0800

    Update reader.js

--- resources/reader.js
+++ resources/reader.js
@@ -2,7 +2,9 @@ function readCSV(fname) {
   const reader = new FileReader();
   let result;
   reader.onload = function(e){
-    result = reader.result;
+    result = reader.result.split('\n').map(function(row){
+      return row.split(',');
+    });
   }
   reader.readAsText(fname);
   return result;
