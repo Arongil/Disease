4ebbb591fb67dd108fb147c13e9f922bf1abc265
Arongil <lakernewhouse@gmail.com>
2017-11-15 22:00:20 -0500

    Constrain x conversion to WIDTH

--- resources/conversion.js
+++ resources/conversion.js
@@ -1,4 +1,4 @@
 // Gall Peters projection conversion
 function convert_coords(latitude, longitude, WIDTH, HEIGHT) {
-  return new Vector2D(WIDTH * longitude / 180, -HEIGHT/2 * Math.sin(latitude * Math.PI/180));
+  return new Vector2D(WIDTH/2 * longitude / 180, -HEIGHT/2 * Math.sin(latitude * Math.PI/180));
 }
