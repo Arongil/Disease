f1caadb4033bcb407c2c8070b3e89c9e4d8df206
Arongil <lakernewhouse@gmail.com>
2017-11-15 21:47:42 -0500

    Update conversion.js

--- resources/conversion.js
+++ resources/conversion.js
@@ -1,5 +1,4 @@
 // Gall Peters projection conversion
-const RADIUS = 6378.1 /* radius of Earth in kilometers */, sq2 = Math.sqrt(2);
 function convert_coords(latitude, longitude, WIDTH, HEIGHT) {
-  return new Vector2D(WIDTH * longitude / (180 * sq2), HEIGHT/2 * Math.sin(latitude));
+  return new Vector2D(WIDTH * longitude / 180, -HEIGHT/2 * Math.sin(latitude));
 }
