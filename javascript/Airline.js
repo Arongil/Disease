db8208a0420a7f8cc1abf8dcba59336172d6e75d
Arongil <lakernewhouse@gmail.com>
2017-11-17 10:24:09 -0500

    Airline.distanceTo(city)
    Agents now prefer flying to closer cities. When an airline finds a flight, it picks two cities based on their population, then returns the closer one.

--- javascript/Airline.js
+++ javascript/Airline.js
@@ -4,14 +4,34 @@ class Airline {
     this.city = city;
   }
   
+  distanceTo(otherCity) {
+    // Credit to https://www.movable-type.co.uk/scripts/latlong.html
+    var R = 6371e3; // metres
+    var lat1 = this.city.latitude * Math.PI/180;
+    var lat2 = otherCity.latitude * Math.PI/180;
+    var latDiff = (lat2-lat1);
+    var lonDiff = (otherCity.longitude - this.city.longitude) * Math.PI/180;
+
+    var a = Math.sin(latDiff/2) * Math.sin(latDiff/2) +
+            Math.cos(lat1) * Math.cos(lat2) *
+            Math.sin(lonDiff/2) * Math.sin(lonDiff/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    return R * c;
+  }
+  
   findFlight() {
     // Lower population cities don't have flights available as often.
     if (Math.random() * this.city.population < 1e6)
       return;
-    // The algorithm chooses cities with higher populations more frequently.
-    var personIndex = Math.random() * GC.WORLDPOPULATION;
-    for (var i = -1; personIndex > 0; i++, personIndex -= GC.cities[i].population);
-    return GC.cities[i];
+    // Choose cities with higher populations more frequently. Of 2 cities chosen like this, return the closer one.
+    var choices = [];
+    while (choices.length < 2) {
+      var personIndex = Math.random() * GC.WORLDPOPULATION;
+      for (var i = -1; personIndex > 0; i++, personIndex -= GC.cities[i].population);
+      choices.push({"city": GC.cities[i], "distance": this.distanceTo(GC.cities[i])});
+    }
+    return choices[0].distance < choices[1].distance ? choices[0].city : choices[1].city;
   }
   
 }
