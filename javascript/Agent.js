6fd6458699d2685377684bb92ea50cbfa5875934
Arongil <lakernewhouse@gmail.com>
2017-11-17 11:24:45 -0500

    Fix: this -> this.city for origin of geodesic

--- javascript/Agent.js
+++ javascript/Agent.js
@@ -24,7 +24,7 @@ class Agent {
   
   fly(destination) {
     stroke(100, 100, 100);
-    geodesic(this, destination, 20);
+    geodesic(this.city, destination, 20);
 
     this.city.agents.splice(this.city.agents.indexOf(this), 1);
     this.city = destination;
