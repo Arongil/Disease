5f928e10a3cdcc72096300abc1d9ad7e76b940aa
Arongil <lakernewhouse@gmail.com>
2017-11-17 14:09:06 -0800

    Update: canvas centering

--- javascript/main.js
+++ javascript/main.js
@@ -23,9 +23,9 @@ function resize() {
     canvas.height = 7/8 * window.innerHeight;
     canvas.width = canvas.height * ASPECTRATIO;
   }
-  document.getElementById("canvas").style.marginLeft = 1/10 * window.innerWidth + "px";
-  document.getElementById("canvas").style.marginRight = 1/10 * window.innerWidth + "px";
-  document.getElementById("canvas").style.marginTop = 1/20 * window.innerWidth + "px";
+  document.getElementById("canvas").style.marginLeft = (window.innerWidth - canvas.width)/2 + "px";
+  document.getElementById("canvas").style.marginRight = (window.innerWidth - canvas.width)/2 + "px";
+  document.getElementById("canvas").style.marginTop = (window.innerWidth - canvas.height)/8 + "px";
   
   WIDTH = canvas.width;
   HEIGHT = canvas.height;
