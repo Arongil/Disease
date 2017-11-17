c2eecbd514c5b128fdb7766f8e8dbbf6a3fd647a
Arongil <lakernewhouse@gmail.com>
2017-11-17 11:29:32 -0500

    Update Agent.js

--- javascript/Agent.js
+++ javascript/Agent.js
@@ -24,7 +24,7 @@ class Agent {
   
   fly(destination) {
     stroke(100, 100, 100);
-    geodesic(this.city, destination, 20);
+    geodesic(this.city, destination, 20, WIDTH, HEIGHT);
 
     this.city.agents.splice(this.city.agents.indexOf(this), 1);
     this.city = destination;
