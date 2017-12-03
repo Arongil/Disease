46ac760a35e9633501101ad0b68751265d4ec570
Arongil <lakernewhouse@gmail.com>
2017-12-03 11:10:21 -0500

    InfoCard Expanded slightly

--- javascript/City.js
+++ javascript/City.js
@@ -37,7 +37,7 @@ class City {
   
   infoCard() {
     fill(0, 0, 0, 0.6);
-    rect(this.pos.x, this.pos.y + this.radius + HEIGHT/7, WIDTH/4, HEIGHT/3.5);
+    rect(this.pos.x, this.pos.y + this.radius + HEIGHT*0.15, WIDTH/4, HEIGHT*0.3);
     fill(255, 255, 255);
     textSize(HEIGHT/28);
     text("-" + this.name + "-", this.pos.x, this.pos.y + this.radius + 1.2*HEIGHT/30);
