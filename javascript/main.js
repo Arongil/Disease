9bc50d64c8e37a81a6bddd88f9cab8b69387c086
the-inhuman-account <31903083+the-inhuman-account@users.noreply.github.com>
2017-12-04 13:50:17 -0800

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -16,16 +16,17 @@ var Input = {
 function resize() {
   ctx.translate(-HALFWIDTH, -HALFHEIGHT);
   
-  canvas.width = 4/5 * window.innerWidth;
+  /*canvas.width = 4/5 * window.innerWidth;
   canvas.height = canvas.width / ASPECTRATIO;
   if (canvas.height > 7/8 * window.innerHeight) {
     // If the height is greater than the height of the screen, set it accordingly.
     canvas.height = 7/8 * window.innerHeight;
     canvas.width = canvas.height * ASPECTRATIO;
-  }
-  document.getElementById("canvas").style.marginLeft = (window.innerWidth - canvas.width)/2 + "px";
-  document.getElementById("canvas").style.marginRight = (window.innerWidth - canvas.width)/2 + "px";
-  document.getElementById("canvas").style.marginTop = canvas.height/20 + "px";
+  }*/
+  canvas.width = window.innerWidth * .8;
+  canvas.height = window.innerHeight;
+  document.getElementById("canvas").style.marginLeft = (window.innerWidth - canvas.width) + "px";
+  document.getElementById("canvas").style.marginTop = 0;
   
   WIDTH = canvas.width;
   HEIGHT = canvas.height;
