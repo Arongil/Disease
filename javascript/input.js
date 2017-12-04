c6be7fd22f5a5f6652642a571d1814e21e30a33c
Arongil <lakernewhouse@gmail.com>
2017-12-04 14:49:24 -0800

    Update input.js

--- javascript/input.js
+++ javascript/input.js
@@ -15,7 +15,7 @@ function initInput(canvas) {
   document.onmousemove = function(e) {
     var x = e.clientX - window.innerWidth/2;
 
-    var y = e.clientY - HALFHEIGHT - HEIGHT/20;
+    var y = e.clientY - HALFHEIGHT - HEIGHT/40;
     Input["mousepos"]["x"] = x;
     Input["mousepos"]["y"] = y;
   };
