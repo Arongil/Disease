42a572ad16985fc4ca098cf2431fa39a43fa2407
the-inhuman-account <31903083+the-inhuman-account@users.noreply.github.com>
2017-12-05 09:41:20 -0800

    Update input.js

--- javascript/input.js
+++ javascript/input.js
@@ -12,11 +12,26 @@ function initInput(canvas) {
   canvas.onmouseup = function(e) {
     Input["click"] = true;
   };
+  let panelID;
+  function panelMouseDown(t)
+  {
+    panelID = t;
+  }
+  function panelMouseUp()
+  {
+    panelID = undefined;
+  }
   document.onmousemove = function(e) {
     var x = e.clientX - window.innerWidth/2;
 
     var y = e.clientY - HALFHEIGHT - HEIGHT/40;
     Input["mousepos"]["x"] = x;
     Input["mousepos"]["y"] = y;
+    
+    if(panelID)
+    {
+      document.getElementById(panelID).style.left = (e.clientX / window.innerWidth) * 100 + '%';
+      document.getElementById(panelID).style.top = (e.clientY / window.innerHeight) * 100 + '%';
+    }
   };
 }
