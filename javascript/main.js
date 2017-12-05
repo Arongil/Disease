f2bf1ce294155959181a926bd0784cf10f6cc791
Arongil <lakernewhouse@gmail.com>
2017-12-04 16:59:10 -0800

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -26,6 +26,7 @@ function resize() {
   document.getElementById("canvas").style.marginLeft = (window.innerWidth - canvas.width)/2 + "px";
   document.getElementById("canvas").style.marginRight = (window.innerWidth - canvas.width)/2 + "px";
   document.getElementById("canvas").style.marginTop = canvas.height/40 + "px";
+  document.getElementById("left-panel").style.width = (window.innerWidth - canvas.width)/2 + "px";
   
   WIDTH = canvas.width;
   HEIGHT = canvas.height;
