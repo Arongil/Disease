a4a394ebf93a759c1366b851067c57a396ac9f7f
Arongil <lakernewhouse@gmail.com>
2017-12-13 17:24:21 -0800

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -37,9 +37,9 @@ function resize() {
   infoPanel.style.right = (window.innerWidth - canvas.width)/2 + "px";
   infoPanel.style.width = canvas.width/2 + "px";
   if (infoPanel.getBoundingClientRect().height > controlPanel.getBoundingClientRect().height)
-    controlPanel.style.height = infoPanel.style.height;
+    controlPanel.style.height = infoPanel.style.height + "px";
   else
-    infoPanel.style.height = controlPanel.style.height;
+    infoPanel.style.height = controlPanel.style.height + "px";
   
   WIDTH = canvas.width;
   HEIGHT = canvas.height;
