bb0fc44e514217265716de0987a4cfde7042e24e
Arongil <lakernewhouse@gmail.com>
2017-12-14 17:26:20 -0800

    Update input.js

--- javascript/input.js
+++ javascript/input.js
@@ -18,20 +18,5 @@ function initInput(canvas) {
     var y = e.clientY - HALFHEIGHT - HEIGHT/40;
     Input["mousepos"]["x"] = x;
     Input["mousepos"]["y"] = y;
-    
-    if(Input["panelID"] !== undefined) {
-      document.getElementById(Input["panelID"]).style.left = (e.clientX / window.innerWidth) * 100 + '%';
-      document.getElementById(Input["panelID"]).style.top = (e.clientY / window.innerHeight) * 100 + '%';
-    }
   };
-  function panelMouseDown(t) {
-    Input["panelID"] = t;
-  }
-  function panelMouseUp() {
-    Input["panelID"] = undefined;
-  }
-  document.getElementById('control-panel').onmousedown = _ => panelMouseDown('control-panel');
-  document.getElementById('info-panel').onmousedown = _ => panelMouseDown('info-panel');
-  document.getElementById('control-panel').onmousedown = panelMouseUp;
-  document.getElementById('info-panel').onmousedown = panelMouseUp;
 }
