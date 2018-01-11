986af6f1e735357f99223ea66caa7168c47705ed
Arongil <lakernewhouse@gmail.com>
2018-01-10 19:42:27 -0900

    Update Agent.js

--- javascript/Agent.js
+++ javascript/Agent.js
@@ -32,9 +32,12 @@ class Agent {
     if (this.city.agents.length - this.city.infectedAgents <= 0)
       return undefined; // There are no susceptible targets.
     
-    var agent;
+    var agent, safeChecks = this.city.agents.length * 10;
     do {
       agent = this.city.agents[Math.floor(this.city.agents.length * Math.random())];
+      safeChecks--;
+      if (safeChecks < 0)
+        return undefined; // Unable to find target; abort.
     } while (agent.healthy === false); // Don't return self!
     return agent;
   }
